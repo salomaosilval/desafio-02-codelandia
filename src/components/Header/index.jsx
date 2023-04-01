03f8d9137707f42fa6284425fc92b6e02f5c0366
salomaosilval <salomaosilval@hotmail.com>
2023-04-01 12:23:32 -0400

    :sparkles: feat: add Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+export const Header = () => {
+  return (
+    <>
+      <div className="headerContainer">
+        <div className="discoutContainer">
+          <p className="discount">Ganhe R$ 10,00 de desconto no frete</p>
+        </div>
+        <div className="titleContainer">
+          <p className="title">JordanShoes</p>
+        </div>
+      </div>
+    </>
+  );
+};

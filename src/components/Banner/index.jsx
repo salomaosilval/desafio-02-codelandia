14772b668ee71f85c7770afc2fb3574f6bfb909d
salomaosilval <salomaosilval@hotmail.com>
2023-04-01 12:43:45 -0400

    :sparkles: feat: add Banner

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+export const Banner = () => {
+  return (
+    <>
+      <div className="bannerContainer">
+        <div className="bannerContent">
+          <h1 className="bannerTitle">A melhor loja de Jordan</h1>
+          <p className="desc">
+            O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};

57a1d8a6a4c230f3b775ad975f7656c288596488
salomaosilval <salomaosilval@hotmail.com>
2023-04-01 13:26:56 -0400

    :sparkles: feat: add Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import "./styles.scss";
+
+export const Footer = () => {
+  return (
+    <>
+      <div className="footerContainer">
+        <p className="footerText">
+          Todos os direitos reservados a{" "}
+          <a href="https://www.nike.com.br/" target="_blank">
+            Nike
+          </a>
+          .
+        </p>
+      </div>
+    </>
+  );
+};
